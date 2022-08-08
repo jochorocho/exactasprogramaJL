35caa4724bd1ecdefd01101e4fa37430bb7f46fb
jochorocho <31703483+jochorocho@users.noreply.github.com>
2022-08-07 23:45:15 -0300

    Update propagacion_fuegos.py

--- propagacion_fuegos.py
+++ propagacion_fuegos.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 """
-Created on Wed Aug  3 19:05:02 2022
+Spyder Editor
 
-@author: clinux01
+This is a temporary script file.
 """
 
 import random
 import statistics
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -62,24 +64,38 @@ def limpieza(bosque):
         if bosque[i] == -1:
             bosque[i] = 0
     return bosque
-
-def dinamica(n, a, p, f): #n = numero de celdas, a = años de repeticion, p = probabilidad de brotes, f = probabilidad de fuego 
+                          
+                          
+#n = numero de celdas, a = años de repeticion, p = probabilidad de brotes, f = probabilidad de fuego
+def dinamica(n, a, p, f):
     count_list = []
     bosque = generar_bosque_vacio(n)
     for t in range (1, a, 1):
-        count_arboles = 0
+        count_tipo(bosque, 1)
         generar_brotes(bosque, p)
         generar_rayos(bosque, f)
         propagacion(bosque)
         limpieza(bosque)
-        count_list.append(count_tipo(bosque, 1)
+        count_list.append(count_tipo(bosque, 1))
     return statistics.mean(count_list)
-                          
-def graficar_dinamicas(intervalo_p, dinamica):
-    x_axis = np.arrange(0, (1+intervalo_p), intervalo_p)
-    y_axis = dinamica(10,500, invervalo_p, 0.02)
+
+
+
+def graficar_dinamicas(intervalo_p, n, a, f):
+    x_axis = np.arange(0, 1, intervalo_p)
+    y_axis = []
+    for i in range (len(x_axis)):
+        valor_y = dinamica(n, a, x_axis[i], f)
+        y_axis.append(valor_y)
     plt.plot(x_axis, y_axis)
-             
+    
+
+#intervalo de p, numero de celdas, años de repeticion, probabilidad de fuego
+print(graficar_dinamicas(0.02, 100, 500, 0.5))
+
+
+
+
 """
 b_1 = [1, 1, 1, -1, 0, 0, 0, -1, 1, 0]
 print(propagacion(b_1))
@@ -89,8 +105,9 @@ print(propagacion(b_2))
 
 b_3 = [-1, 1, 0, 1, 0, -1, 1, 0, 0, 1, -1]
 print(propagacion(b_3))
-"""       
-"""  
+
+
+ 
 bosque_t0 = generar_bosque_vacio(10)
 print(bosque_t0)
 bosque_t1 = generar_brotes(bosque, 0.6)
